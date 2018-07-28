8c5fca14252ed27f6f1b3a38689acbdf5828fd89
Vivek Patel <M1044069@mindtree.com>
2018-07-29 01:43:13 +0530

    Updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //private name: string;
+  //private cost: string;
+ // private pic: string;
+  private product :any ={};
+  private products: any[] = [
+   // { name: 'Refrigerator', cost: 1700, pic: '' },
+    { name: 'Television', cost: 4500, pic: '' }
+  ];
+
+  saveProduct() {
+
+    //var product = {
+    // name: this.name,
+     // cost: this.cost,
+     // pic: this.pic
+    //}
+    this.products.push(this.product);
+   // this.name='',
+    this.product={};
+  }
+  deleteProduct(index){
+    this.products.splice(index,1);
+
+  }
+  editProduct(product){
+console.log("record is ",product);
+this.product=product;
+  }
+
 }
